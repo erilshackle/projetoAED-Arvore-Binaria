6b06dd371cab1456c9b265a91711db06eb78c479
Eril T.S. Carvalho <74083037+erilshackle@users.noreply.github.com>
2023-06-16 20:51:16 -0100

    Frist Upload
    Project from DevC++

---
+++ Subject_Manager_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SUBJECT_MANAGER_PRIVATE_H
+#define SUBJECT_MANAGER_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Eril carvalho"
+#define FILE_VERSION	"1.0.0.0"
+#define FILE_DESCRIPTION	"Projeto final A.E.D desenvolvido por Erilando Carvalho."
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"subject manager"
+#define PRODUCT_NAME	"Subject Manager"
+#define PRODUCT_VERSION	"1.0.0.0"
+
+#endif /*SUBJECT_MANAGER_PRIVATE_H*/
